ffc36efb4f8ed6b7d50192757c469ac5caeef48c
mikayel <admin@zzillasoft.com>
2019-02-22 16:05:32 +0400

    add configuration flags

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # cfliteapi
 cloudflare lite api (list, create, update, delete)
 
+Create config file:        cflite -config -email=some@mail.com -xkey=XAuthKey
+
+Work without config file:  cflite -email=some@mail.com -xkey=XAuthKey -domain=example.com -name=test.example.com -type=A  -content=12.13.14.15   -proxy=true  -akt=create
+
+If config file exist
 Create:    cflite -domain=example.com -name=test.example.com -type=A  -content=12.13.14.15   -proxy=true  -akt=create"
 Create MX: cflite -domain=example.com -name=mx.example.com   -type=MX -content=mx.server.com -proxy=10    -akt=create"
 Update:    cflite -domain=example.com -name=test.example.com -type=A  -content=12.13.14.16   -proxy=false -akt=update"
--- cflite.go
+++ cflite.go
@@ -17,32 +17,11 @@ func writheConf(email string, key string) error {
 	config.Data.XAuthKey = key
 	confJSON, _ := json.Marshal(config)
 	err := ioutil.WriteFile("./cf.conf", confJSON, 644)
-	//fmt.Println(config, confJSON)
 	return err
 }
 
 func readConfig() error {
 	var conf = new(cfst.CFConf)
-	// if _, err := os.Stat("./cf.conf"); os.IsNotExist(err) {
-	// 	reader := bufio.NewReader(os.Stdin)
-	// 	fmt.Println("Email: ")
-	// 	cffc.Email, _ = reader.ReadString('\n')
-	// 	cffc.Email = strings.TrimSpace(cffc.Email)
-	// 	fmt.Println("Key: ")
-	// 	cffc.XAuthKey, _ = reader.ReadString('\n')
-	// 	cffc.XAuthKey = strings.TrimSpace(cffc.XAuthKey)
-	// 	fmt.Println("Config file not found, create one? (Y/n)")
-
-	// 	cmdString, err := reader.ReadString('\n')
-	// 	if err != nil {
-	// 		fmt.Fprintln(os.Stderr, err)
-	// 	}
-	// 	strings.TrimSpace(cmdString)
-	// 	strings.ToUpper(cmdString)
-	// 	if cmdString != "N" {
-	// 		writheConf(cffc.Email, cffc.XAuthKey)
-	// 	}
-	// } else
 	if file, err := os.Open("./cf.conf"); err == nil {
 		decoder := json.NewDecoder(file)
 		if err := decoder.Decode(&conf); err != nil {
@@ -55,9 +34,6 @@ func readConfig() error {
 }
 
 func init() {
-	// if _, err := os.Stat("./cf.conf"); os.IsNotExist(err) {
-	// 	fmt.Println("You need to enter your email and authorization key for authorization in cloudflare")
-	// }
 	readConfig()
 }
 func main() {
@@ -69,14 +45,28 @@ func main() {
 	contentPtr := flag.String("content", "", "content of resource")
 	aktPtr := flag.String("akt", "", "create akt")
 	proxyPtr := flag.String("proxy", "", "proxy")
-	createConfigPtr := flag.Bool("config", true, "create config")
+	createConfigPtr := flag.Bool("config", false, "create config")
+	helpPtr := flag.Bool("help", false, "help")
+
 	flag.Parse()
+
 	if *createConfigPtr == true && *emailPtr != "" && *xkeyPtr != "" {
 		writheConf(*emailPtr, *xkeyPtr)
 	}
 	if *emailPtr != "" && *xkeyPtr != "" {
 		cffc.Email = *emailPtr
 		cffc.XAuthKey = *xkeyPtr
+	}
+	if *helpPtr {
+		fmt.Println("Create config file:        cflite -config -email=some@mail.com -xkey=XAuthKey")
+		fmt.Println("Work without config file:  cflite -email=some@mail.com -xkey=XAuthKey -domain=example.com -name=test.example.com -type=A  -content=12.13.14.15   -proxy=true  -akt=create")
+		fmt.Println("If config file exist")
+		fmt.Println("Create:    cflite -domain=example.com -name=test.example.com -type=A  -content=12.13.14.15   -proxy=true  -akt=create")
+		fmt.Println("Create MX: cflite -domain=example.com -name=mx.example.com   -type=MX -content=mx.server.com -proxy=10    -akt=create")
+		fmt.Println("Update:    cflite -domain=example.com -name=test.example.com -type=A  -content=12.13.14.16   -proxy=false -akt=update")
+		fmt.Println("Delete:    cflite -domain=example.com -name=test.example.com -type=A  -akt=delete")
+		fmt.Println("List:      cflite -domain=example.com -akt=list")
+
 	}
 	switch *aktPtr {
 	case "create":
@@ -92,10 +82,5 @@ func main() {
 		//cffc.ListRecords(*domainPtr, *namePtr, *contentPtr, *restypePtr, *proxyPtr)
 		cffc.Akter(3, *domainPtr, *namePtr, *contentPtr, *restypePtr, *proxyPtr)
 	default:
-		fmt.Println("Create:    cflite -domain=example.com -name=test.example.com -type=A  -content=12.13.14.15   -proxy=true  -akt=create")
-		fmt.Println("Create MX: cflite -domain=example.com -name=mx.example.com   -type=MX -content=mx.server.com -proxy=10    -akt=create")
-		fmt.Println("Update:    cflite -domain=example.com -name=test.example.com -type=A  -content=12.13.14.16   -proxy=false -akt=update")
-		fmt.Println("Delete:    cflite -domain=example.com -name=test.example.com -type=A  -akt=delete")
-		fmt.Println("List:      cflite -domain=example.com -akt=list")
 	}
 }
