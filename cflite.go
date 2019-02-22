706a6ad2a24611f051bc402c4e98ff7454204a7b
mikayel <admin@zzillasoft.com>
2019-02-22 12:29:08 +0400

    change startup flags

--- cflite.go
+++ cflite.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	cffc "cfmain/cffunc"
 	cfst "cfmain/cfstructs"
 	"encoding/json"
@@ -9,7 +8,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
-	"strings"
 )
 
 func writheConf(email string, key string) error {
@@ -25,26 +23,27 @@ func writheConf(email string, key string) error {
 
 func readConfig() error {
 	var conf = new(cfst.CFConf)
-	if _, err := os.Stat("./cf.conf"); os.IsNotExist(err) {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Println("Email: ")
-		cffc.Email, _ = reader.ReadString('\n')
-		cffc.Email = strings.TrimSpace(cffc.Email)
-		fmt.Println("Key: ")
-		cffc.XAuthKey, _ = reader.ReadString('\n')
-		cffc.XAuthKey = strings.TrimSpace(cffc.XAuthKey)
-		fmt.Println("Config file not found, create one? (Y/n)")
+	// if _, err := os.Stat("./cf.conf"); os.IsNotExist(err) {
+	// 	reader := bufio.NewReader(os.Stdin)
+	// 	fmt.Println("Email: ")
+	// 	cffc.Email, _ = reader.ReadString('\n')
+	// 	cffc.Email = strings.TrimSpace(cffc.Email)
+	// 	fmt.Println("Key: ")
+	// 	cffc.XAuthKey, _ = reader.ReadString('\n')
+	// 	cffc.XAuthKey = strings.TrimSpace(cffc.XAuthKey)
+	// 	fmt.Println("Config file not found, create one? (Y/n)")
 
-		cmdString, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Fprintln(os.Stderr, err)
-		}
-		strings.TrimSpace(cmdString)
-		strings.ToUpper(cmdString)
-		if cmdString != "N" {
-			writheConf(cffc.Email, cffc.XAuthKey)
-		}
-	} else if file, err := os.Open("./cf.conf"); err == nil {
+	// 	cmdString, err := reader.ReadString('\n')
+	// 	if err != nil {
+	// 		fmt.Fprintln(os.Stderr, err)
+	// 	}
+	// 	strings.TrimSpace(cmdString)
+	// 	strings.ToUpper(cmdString)
+	// 	if cmdString != "N" {
+	// 		writheConf(cffc.Email, cffc.XAuthKey)
+	// 	}
+	// } else
+	if file, err := os.Open("./cf.conf"); err == nil {
 		decoder := json.NewDecoder(file)
 		if err := decoder.Decode(&conf); err != nil {
 			return err
@@ -56,19 +55,29 @@ func readConfig() error {
 }
 
 func init() {
-	if _, err := os.Stat("./cf.conf"); os.IsNotExist(err) {
-		fmt.Println("You need to enter your email and authorization key for authorization in cloudflare")
-	}
+	// if _, err := os.Stat("./cf.conf"); os.IsNotExist(err) {
+	// 	fmt.Println("You need to enter your email and authorization key for authorization in cloudflare")
+	// }
 	readConfig()
 }
 func main() {
+	emailPtr := flag.String("email", "", "cloudflare email")
+	xkeyPtr := flag.String("xkey", "", "cloudflare xkey")
 	domainPtr := flag.String("domain", "", "Domain name")
 	namePtr := flag.String("name", "", "record name")
 	restypePtr := flag.String("type", "", "resource type")
 	contentPtr := flag.String("content", "", "content of resource")
 	aktPtr := flag.String("akt", "", "create akt")
 	proxyPtr := flag.String("proxy", "", "proxy")
+	createConfigPtr := flag.Bool("config", true, "create config")
 	flag.Parse()
+	if *createConfigPtr == true && *emailPtr != "" && *xkeyPtr != "" {
+		writheConf(*emailPtr, *xkeyPtr)
+	}
+	if *emailPtr != "" && *xkeyPtr != "" {
+		cffc.Email = *emailPtr
+		cffc.XAuthKey = *xkeyPtr
+	}
 	switch *aktPtr {
 	case "create":
 		//cffc.CreateRecord(*domainPtr, *namePtr, *contentPtr, *restypePtr, *proxyPtr)
@@ -90,4 +99,3 @@ func main() {
 		fmt.Println("List:      cflite -domain=example.com -akt=list")
 	}
 }
-
